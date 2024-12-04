dcf28676f6346a094b16bb0b56db9afe5b92107a
nikhilshet <nikhilshet17@gmail.com>
2024-12-04 19:06:37 +0530

    changes

--- src/components/DiveIn.jsx
+++ src/components/DiveIn.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import arrow from '../assets/images/Icons/arrow.svg'
 function DiveIn() {
 
-    const locations = ["Abu Dhabi","Dubai","Sharjah & Ajman","Fujairah","Ras Al Khaimah","Umm Al Quwain"]
+    const locations = ["Abu Dhabi","Dubai","Sharjah & Ajman","Fujairah","Ras Al Khaimah","Umm Al Quwain"] 
+    // transition-all duration-200 hover:bg-[#E7760D] hover:text-white
 
     const locationElements = locations.map((ele , index)=>{
         return(
-            <div key={index}  className='w-64 h-16 rounded-md bg-white border-1 flex justify-center items-center border-[#E7760D] text-black'>
+            <div key={index}  className='w-64 h-16 rounded-md bg-white border-1 flex justify-center items-center transition-all duration-200 border-[#E7760D] text-black hover:bg-[#E7760D] hover:text-white'>
             <p className='text-lg'>{ele}</p>
             </div>
         )
@@ -17,7 +18,7 @@ function DiveIn() {
         <div className=' w-full flex flex-col justify-center space-y-6 items-center xl:w-1/3 '>
             <h2 className='text-center text-3xl font-bold' >Ready to dive into HABOT?</h2>
             <p className='text-center leading-6 text-md font-light w-[90%]'> Signing up with HABOT opens the door to a world of new opportunities and potential for business growth. Gain access to a vibrant community of like-minded individuals, unlock valuable resources, and take the first step towards realizing your entrepreneurial dreams.</p>
-            <button className='bg-green-700 text-white text-md font-semibold py-3 px-3 flex items-center rounded-md mb-4'>Sign up Today !<img className='ml-4'  src={arrow}></img></button>
+            <button className='bg-green-700 text-white text-md font-semibold py-3 px-3 flex items-center rounded-md mb-4 transition-all duration-200 hover:bg-white hover:text-green-600 hover:border-1 hover:border-green-600'>Sign up Today !<img className='ml-4'  src={arrow}></img></button>
         </div>
         <div className='grid gap-5 lg:grid-cols-2 xl:self-center'>
             {locationElements}
--- src/components/Supplier.jsx
+++ src/components/Supplier.jsx
@@ -5,10 +5,10 @@ function Supplier() {
     <>
         <section className='w-full bg-[#E8FBFF] mb-24 py-24 flex flex-col justify-between items-center xl:flex-row xl:px-32 '>
             <h2 className='text-3xl mb-12 font-semibold xl:text-4xl'>Let Suppliers&nbsp; 
-                <span className='relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-[#EB7150] inline-block'>Find You</span>
+                <span className='relative after:absolute after:left-0 after:bottom-0 after:w-full after:h-1 after:bg-[#EB7150] inline-block '>Find You</span>
             </h2>
 
-            <button className='bg-[#EB7150] text-white text-lg rounded-lg px-3 py-2 xl:px-6 xl:py-4'>Get Verified</button>
+            <button className='bg-[#EB7150] text-white text-lg rounded-lg px-3 py-2 xl:px-6 xl:py-4 transition-all duration-200 hover:bg-white hover:text-[#E7760D] hover:border-1 hover:border-[#E7760D] '>Get Verified</button>
         </section>
     </>
   )
